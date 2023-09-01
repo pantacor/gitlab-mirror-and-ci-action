5fb0ba116b6ba8fd663d4e7821e399f556729814
Aníbal Portero Hermida <anibal.portero@pantacor.com>
2023-09-01 16:40:17 +0200

    move safe directory setting before first git command

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,6 +24,8 @@ urlencode() (
 DEFAULT_POLL_TIMEOUT=10
 POLL_TIMEOUT=${POLL_TIMEOUT:-$DEFAULT_POLL_TIMEOUT}
 
+sh -c "git config --global --add safe.directory $PWD"
+
 git checkout "${GITHUB_REF:11}"
 
 branch="$(git symbolic-ref --short HEAD)"
@@ -32,7 +34,6 @@ branch_uri="$(urlencode ${branch})"
 sh -c "git config --global credential.username $GITLAB_USERNAME"
 sh -c "git config --global core.askPass /cred-helper.sh"
 sh -c "git config --global credential.helper cache"
-sh -c "git config --global --add safe.directory $PWD"
 sh -c "git remote add mirror $*"
 sh -c "echo pushing to $branch branch at $(git remote get-url --push mirror)"
 git_status=$(sh -c "git push mirror -f $branch" >& /dev/stdout)
